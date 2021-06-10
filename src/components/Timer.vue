0663d114a810675097cdfdc9cbbb35a3b3c7ef00
Li Hanyang <li.hanyang@foxmail.com>
2021-06-10 22:56:06 +0800

    feat: 倒计时时间设定轮盘

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,5 +1,7 @@
 <template>
+<!-- 组件根元素 -->
 <div id="timer">
+  <!-- 倒计时面板 -->
   <div id="timer-dash">
     <!-- 倒计时 -->
     <div id="count-down">
@@ -47,6 +49,57 @@
         <span class="digits">{{ alarmMinutes }}</span>
       </span>
     </div>
+    <!-- 计时时间设置 -->
+    <div id="time-setting" :class="{hidden: timer}">
+      <div id="wheels-container">
+        <div 
+            :id="`${part.name}-digits-container`"
+            class="wheel-part"
+            v-for="part in wheels"
+            :key="part.label">
+          <div
+              class="wheel-digits"
+              :class="{
+                'moved-before': part.isMoving.before,
+                'moved-after': part.isMoving.after,
+              }"
+              v-for="digit in part.shownDigits"
+              :key="digit">
+            {{ digit }}
+          </div>
+        </div>
+      </div>
+      <div id="wheel-window">
+        <div 
+            class="window-part"
+            v-for="part in wheels"
+            :key="part.label">
+          <div class="part-digits-container">
+            <div
+                class="part-digits"
+                :class="{
+                  'moved-before': part.isMoving.before,
+                  'moved-after': part.isMoving.after,
+                }"
+                :ref="part.name + 'Window'"
+                :data-before="part.shownDigits[3]"
+                :data-after="part.shownDigits[5]">
+              {{ part.shownDigits[4] }}
+            </div>
+          </div>
+          <div class="part-label">
+            <span>
+              {{ part.label }}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div id="controlers-container">
+        <div class="controler" ref="hoursCtl"></div>
+        <div class="controler" ref="minutesCtl"></div>
+        <div class="controler" ref="secondsCtl"></div>
+      </div>
+    </div>
   </div>
   
   <!-- 控制按钮 -->
@@ -88,10 +141,6 @@ export default {
       // 基础尺寸
       size: 300,
       strokeWidth: 6,
-      // 设置的倒计时时间
-      hours: 0,
-      minutes: 0,
-      seconds: 5,
       // 倒计时器进程
       timer: null,
       leftMilliSeconds: 0,
@@ -101,6 +150,39 @@ export default {
       btnTags: new Map([["stopped", "开始计时"], ["running", "暂停"], ["paused", "继续"]]),
       // 计时器触发
       triggered: false,
+      // 倒计时时间设置轮盘
+      wheels: {
+        hoursWheel: {
+          name: "hours",
+          label: "小时",
+          maxDigit: 23,
+          shownDigits: [20, 21, 22, 23, 0, 1, 2, 3, 4],
+          isMoving: {
+            before: false,
+            after: false,
+          },
+        },
+        minutesWheel: {
+          name: "minutes",
+          label: "分钟",
+          maxDigit: 59,
+          shownDigits: [56, 57, 58, 59, 0, 1, 2, 3, 4],
+          isMoving: {
+            before: false,
+            after: false,
+          },
+        },
+        secondsWheel: {
+          name: "seconds",
+          label: "秒",
+          maxDigit: 59,
+          shownDigits: [56, 57, 58, 59, 0, 1, 2, 3, 4],
+          isMoving: {
+            before: false,
+            after: false,
+          },
+        },
+      },
     }
   },
   computed: {
@@ -113,6 +195,16 @@ export default {
     },
     circumference() {
       return 2 * this.radius * Math.PI;
+    },
+    //设置的倒计时时间
+    hours() {
+      return this.wheels.hoursWheel.shownDigits[4];
+    },
+    minutes() {
+      return this.wheels.minutesWheel.shownDigits[4];
+    },
+    seconds() {
+      return this.wheels.secondsWheel.shownDigits[4];
     },
     // 倒计时器进程计算值
     totalMilliSeconds() {
@@ -168,6 +260,19 @@ export default {
     controlBtnTag() {
       return this.btnTags.get(this.controlBtnState);
     },
+    // 倒计时时间设置计算值
+    hoursMoving() {
+      const states = this.wheels.hoursWheel.isMoving;
+      return states.before || states.after;
+    },
+    minutesMoving() {
+      const states = this.wheels.minutesWheel.isMoving;
+      return states.before || states.after;
+    },
+    secondsMoving() {
+      const states = this.wheels.secondsWheel.isMoving;
+      return states.before || states.after;
+    },
   },
   methods: {
     cancel() {
@@ -211,13 +316,47 @@ export default {
     },
     alarmOff() {
       this.triggered = false;
+    },
+  },
+  mounted() {
+    for (let wheel in this.wheels) {
+      wheel = this.wheels[wheel]
+      
+      // 给倒计时时间设置轮盘控制器绑定鼠标滚轮事件
+      this.$refs[wheel.name + 'Ctl'].addEventListener('wheel', event => {
+        if (!this.timer && !this[this.name +'Moving']) {
+          if (event.deltaY < 0) {
+            wheel.isMoving.before = true;
+          } else if (event.deltaY > 0) {
+            wheel.isMoving.after = true;
+          }
+        }
+      });
+
+      // 给倒计时时间设置轮盘绑定过渡动画处理器
+      this.$refs[wheel.name + 'Window'].addEventListener('transitionend', () => {
+        if (wheel.isMoving.before) {
+          wheel.shownDigits.pop();
+          const first = wheel.shownDigits[0];
+          wheel.shownDigits.unshift(first > 0 ? first - 1 : wheel.maxDigit);
+          wheel.isMoving.before = false;
+        }
+        if (wheel.isMoving.after) {
+          wheel.shownDigits.shift();
+          const last = wheel.shownDigits[wheel.shownDigits.length - 1];
+          wheel.shownDigits.push(last < wheel.maxDigit ? last + 1 : 0);
+          wheel.isMoving.after = false;
+        }
+      });
     }
-  }
+  },
 }
 </script>
 
 
 <style lang="scss">
+@use "sass:math";
+
 @font-face {
   font-family: 'Uni Sans';
   src: url(../assets/fonts/uni-sans.thin-caps.otf);
@@ -239,7 +378,6 @@ body {
 }
 
 #timer {
-  outline: 1px solid palevioletred;
   margin: 0 auto;
   padding-top: $component-top;
   width: $component-width;
@@ -435,6 +573,210 @@ body {
     &.paused {
       color: #282828;
     }
+  }
+}
+
+#time-setting {
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 300px;
+  background: #000;
+  opacity: 1;
+  transition: opacity .2s ease-in;
+
+  $wheel-height: 13rem;
+  $digits-width: 2rem;
+  
+  #wheels-container {
+    position: absolute;
+    left: 45.2%;
+    top: 45%;
+    transform: translate(-50%, -50%);
+    box-sizing: border-box;
+    height: $wheel-height;
+    width: 14rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .wheel-part {
+      height: 2rem;
+      width: $digits-width;
+      color: #909090;
+      font-family: 'HelveticaNeue-UltraLight', 'Helvetica Neue UltraLight', 'Helvetica Neue', Helvetica, sans-serif;
+      font-size: 1.2rem;
+      font-weight: 300;
+      line-height: 2rem;
+      text-align: right;
+      position: relative;
+      
+      .wheel-digits {
+        position: absolute;
+        left: 0;
+        right: 0;
+      }
+
+      @function computeTransform($normIdx, $base-x, $delta-x, $base-y, $delta-y, $base-s, $delta-s) {
+        @if $normIdx == 0 {
+          @return translate(0, 0) scale(1);
+        } @else {
+          $symbol-y: 0 - math.div($normIdx, math.abs($normIdx));
+          $normIdx: math.abs($normIdx) - 1;
+          $computed-x: $base-x;
+          $computed-y: $base-y * $symbol-y;
+          $computed-s: $base-s;
+          $value: null;
+          @for $j from 0 through $normIdx {
+            @if $j > 0 {
+              $times: math.pow(2, ($j - 1));
+              $computed-x: $computed-x + ($delta-x * $times);
+              $computed-y: $computed-y + ($delta-y * $times * $symbol-y);
+              $computed-s: $computed-s + ($delta-s * $times);
+            }
+            $value: $value + translate(#{$computed-x}rem, #{$computed-y}rem) scaleY(#{$computed-s});
+          }
+          @return $value;
+        }
+      }
+
+      @mixin makeDigit($visiable, $animated) {
+        opacity: if($visiable, 1, 0);
+        @if $animated {
+          transition:  
+            transform .2s ease-in,
+            opacity .2s ease-in;
+        }
+      }
+
+      @mixin makeWheel($base-x, $delta-x, $base-y, $delta-y, $base-s, $delta-s) {
+        @for $i from 1 through 9 {
+          .wheel-digits:nth-child(#{$i}) {
+            transform: computeTransform((5 - $i), $base-x, $delta-x, $base-y, $delta-y, $base-s, $delta-s);
+            @include makeDigit(true, false);
+          }
+
+          .wheel-digits.moved-before:nth-child(#{$i}) {
+            transform: computeTransform((4 - $i), $base-x, $delta-x, $base-y, $delta-y, $base-s, $delta-s);
+            @if $i == 1 {
+              @include makeDigit(false, true);
+            } @else {
+              @include makeDigit(true, true);
+            }
+          }
+
+          .wheel-digits.moved-after:nth-child(#{$i}) {
+            transform: computeTransform((6 - $i), $base-x, $delta-x, $base-y, $delta-y, $base-s, $delta-s);
+            @if $i == 9 {
+              @include makeDigit(false, true);
+            } @else {
+              @include makeDigit(true, true);
+            }
+          }
+        }
+      }
+
+      &#hours-digits-container {
+        @include makeWheel(.01, .02, 1.8, 0.1, .9, -.1);
+      }
+
+      &#minutes-digits-container {
+        @include makeWheel(0, 0, 1.8, 0.1, .9, -.1);
+      }
+
+      &#seconds-digits-container {
+        @include makeWheel(-.01, -.02, 1.8, 0.1, .9, -.1);
+      }
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(180deg, rgba(0, 0, 0, .5) 0%, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, .5) 100%);
+    }
+  }
+
+  #wheel-window {
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    transform: translate(-50%, -50%);
+    box-sizing: border-box;
+    height: 2rem;
+    width: $component-width - ($component-top * 3);
+    padding-left: .4rem;
+    padding-right: 1.6rem;
+    background-color: #333;
+    border-radius: .4rem;
+    display: flex;
+    justify-content: space-between;
+    font-size: 1rem;
+    line-height: 2rem;
+    overflow: hidden;
+
+    .window-part {
+      display: flex;
+
+      .part-digits-container {
+        width: $digits-width;
+        margin-right: .3rem;
+        text-align: right;
+        font-family: 'HelveticaNeue-UltraLight', 'Helvetica Neue UltraLight', 'Helvetica Neue', Helvetica, sans-serif;
+        font-size: 1.2rem;
+        font-weight: 300;
+
+        .part-digits{
+          transform: translateY(-2rem);
+
+          &.moved-before {
+            transform: translateY(0);
+            transition: transform .2s ease-in;
+          }
+
+          &.moved-after {
+            transform: translateY(-4rem);
+            transition: transform .2s ease-in;
+          }
+
+          &::before {
+            content: attr(data-before);
+            display: block;
+          }
+
+          &::after {
+            content: attr(data-after);
+            display: block;
+          }
+        }
+      }
+
+      .part-label span {
+        vertical-align: -.1rem;
+      }
+    }
+  }
+
+  #controlers-container {
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    transform: translate(-50%, -50%);
+    box-sizing: border-box;
+    height: $wheel-height;
+    width: $component-width - ($component-top * 3);
+    display: flex;
+
+    .controler {
+      flex: 1 1 auto;
+    }
+  }
+
+  &.hidden {
+    opacity: 0;
   }
 }
 </style>
